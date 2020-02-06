c4e88369e23ff78942ed245e723b7a5533fc2d68
hernad <hernad@bring.out.ba>
2020-02-06 19:14:50 +0100

    maven jna

--- 04_java_start/src/main/java/ba/out/Greeting.java
+++ 04_java_start/src/main/java/ba/out/Greeting.java
@@ -2,7 +2,7 @@ package ba.out;
 
 // import from guava
 import com.google.common.primitives.Ints;
-
+import com.sun.jna.Platform;
 
 public class Greeting {
 
@@ -16,6 +16,8 @@ public class Greeting {
             System.out.println( compare(10, 10) );
             System.out.println( compare(100, 200) );
             System.out.println("Hi from out.ba!");
+            System.out.println( "is windows:" + Boolean.toString(Platform.isWindows()));
+
     }
 }
 
