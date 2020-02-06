89db98274402d1bf81799ed0ef6211a058bfc041
hernad <hernad@bring.out.ba>
2020-02-06 15:54:18 +0100

    DLLEXPORT

--- 03_shared_library/lib.cc
+++ 03_shared_library/lib.cc
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "lib.h"
 
+#ifdef _MSC_VER
+   #include <windows.h>
+#endif
+
 extern "C" {
+
+#ifdef _MSC_VER
+  DLLEXPORT
+#endif
   int get_number_plus_2(int number) {
 	printf(SYM);
 	return number + 2;
