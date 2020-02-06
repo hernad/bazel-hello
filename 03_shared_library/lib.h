7004556e3e9e1e7df70411eac500a2d2ffa09b22
hernad <hernad@bring.out.ba>
2020-02-06 16:00:51 +0100

    dll windows

--- 03_shared_library/lib.h
+++ 03_shared_library/lib.h
@@ -1,5 +1,14 @@
 #define SYM  "symbol01"
 
 
+#ifdef COMPILING_DLL
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
+extern "C" DLLEXPORT char *get_time();
+extern "C" DLLEXPORT void say_hello(char *);
+
 
 extern "C" int get_number_plus_2(int number);
