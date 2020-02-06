e1c53a0e50c380a3b03e60ad090b0ad33c0f52c1
hernad <hernad@bring.out.ba>
2020-02-06 19:00:55 +0100

    WORKSPACE: maven_install

--- 04_java_start/src/main/java/ba/out/Greeting.java
+++ 04_java_start/src/main/java/ba/out/Greeting.java
@@ -1,7 +1,20 @@
 package ba.out;
 
+// import from guava
+import com.google.common.primitives.Ints;
+
+
 public class Greeting {
+
+    public static int compare(int a, int b) {
+        return Ints.compare(a, b);
+    }
+
     public static void sayHi() {
+
+            System.out.println( compare(20, 1) );
+            System.out.println( compare(10, 10) );
+            System.out.println( compare(100, 200) );
             System.out.println("Hi from out.ba!");
     }
 }
