1d76ea8d8a95f1879e7abf344b4928d1cf129dc3
hernad <hernad@bring.out.ba>
2020-02-13 17:34:26 +0100

    emulator start

--- 07_android_hello/build.sh
+++ 07_android_hello/build.sh
@@ -6,3 +6,8 @@ export ANDROID_HOME=$HOME/Android/Sdk
 # https://docs.bazel.build/versions/master/tutorial/android-app.html
 
 bazel build //App-A-Android:AppA-Android --subcommands
+
+
+bazel mobile-install //App-A-Android:AppA-Android 
+
+
